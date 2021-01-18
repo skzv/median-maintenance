674f302c3a2192a1b14d075f010f069328760de3
Alexander Kuznetsov <alexander.kuznetsov96@gmail.com>
2021-01-18 14:51:37 -0500

    Implement and add data

--- src/dev/skz/Main.java
+++ src/dev/skz/Main.java
@@ -3,6 +3,16 @@ package dev.skz;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        MedianMaintainer<Integer> mm = new MedianMaintainer<>();
+        mm.add(1);
+        System.out.println(mm.getMedian());
+        mm.add(2);
+        System.out.println(mm.getMedian());
+        mm.add(3);
+        System.out.println(mm.getMedian());
+        mm.add(4);
+        System.out.println(mm.getMedian());
+        mm.add(5);
+        System.out.println(mm.getMedian());
     }
 }
--- src/dev/skz/MedianMaintainer.java
+++ src/dev/skz/MedianMaintainer.java
@@ -1,5 +1,41 @@
 package dev.skz;
 
-public class MedianMaintainer {
+import java.util.Collections;
+import java.util.PriorityQueue;
 
+public class MedianMaintainer <T extends Comparable<T>> {
+  PriorityQueue<T> hLo = new PriorityQueue<>(Collections.reverseOrder());
+  PriorityQueue<T> hHi = new PriorityQueue<>();
+  T currentMedian = null;
+
+  public void add(T x) {
+    if (hLo.size() == 0) {
+      hLo.add(x);
+    } else if (x.compareTo(hLo.peek()) <= 0) {
+      hLo.add(x);
+    } else if (x.compareTo(hLo.peek()) > 0) {
+      hHi.add(x);
+    }
+    rebalanceIfNecessary();
+    currentMedian = hLo.peek();
+  }
+
+  public T getMedian() {
+    return currentMedian;
+  }
+
+  // hLo will always have more if the total is odd
+  private void rebalanceIfNecessary() {
+    T x;
+
+    while (hHi.size() > hLo.size()) {
+      x = hHi.poll();
+      hLo.add(x);
+    }
+
+    while (hLo.size() - hHi.size() > 1) {
+      x = hLo.poll();
+      hHi.add(x);
+    }
+  }
 }
